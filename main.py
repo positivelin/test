f749799c183c80caf90e1b4437fec47c4d2184b5
sean_lin <pspsean1234@gmail.com>
2022-05-20 11:42:33 +0800

    UVA 10055

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-n = int(input())
-for i in range(n):
-    result = 0
-    temp = list(map(int,input().split(' ')))
-    Case = temp[0]
-    House = sorted(temp[1::])
-
-    if Case % 2 == 0:
-        mid = (int(House[Case//2-1])+int(House[Case//2]))/2
-    else:
-        mid = int(House[Case//2])
-
-    for j in House:
-        result += abs(mid - int(j))
-    # print(mid)
-    print(int(result))
+while 1:
+    check = []
+    try:
+        check = list(map(int,input().split(' ')))
+    except:
+        break
+    print(abs(check[0]-check[1]))
